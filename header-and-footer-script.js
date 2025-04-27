5c53e0e3c04f1e05acab72b6b35937b782d53aed
Scott Enos <scott.enos2@gmail.com>
2025-04-27 17:24:59 -0400

    Initial Commit

---
+++ header-and-footer-script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header').innerHTML = data;
+        })
+        .catch(error => console.error('Error loading header:', error));
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('footer').innerHTML = data;
+        })
+        .catch(error => console.error('Error loading header:', error));
+});
